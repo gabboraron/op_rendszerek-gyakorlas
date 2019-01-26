f2da1d87df31cd978bb905362b7e1d9ceb593d28
gabboraron <gabboraron@gmail.com>
2019-01-26 13:21:44 +0100

    Add files via upload

---
+++ SIGNAL_egy_szulo_egy_gyerek_szulo_signalt_kuld.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+
+void handler(int signumber){
+  printf("Signal with number %i has arrived\n",signumber);
+}
+
+int main(){
+
+  sigset_t sigset;
+  sigemptyset(&sigset); //empty signal set
+  sigaddset(&sigset,SIGTERM); //SIGTERM is in set
+  //sigfillset(&sigset); //each signal is in the set
+  sigprocmask(SIG_BLOCK,&sigset,NULL); //signals in sigset will be blocked
+               //parameters, how: SIG_BLOCK, SIG_UNBLOCK, SIG_SETMASK -   ;
+               //2. parameter changes the signalset to this if it is not NULL,
+               //3.parameter if it is not NULL, the formerly used set is stored here
+    
+  signal(SIGTERM,handler); //signal and handler is connetcted
+  //signal(SIGUSR1,handler); 
+
+  pid_t child=fork();
+  /*if (child>0)
+  {
+    printf("I'm parrent\n");
+    printf("Waits 2 seconds, then send a SIGTERM %i signal (it is blocked)\n",SIGTERM);
+    sleep(2);
+    kill(getppid(),SIGTERM); 
+    printf("I sent it.\n");   
+  }
+  else
+  {
+    wait(NULL);
+    sleep(2);
+    printf("I'm the child wainting for signal.\n");
+
+    sigprocmask(SIG_UNBLOCK,&sigset,NULL); 
+   
+    int status;
+    wait(&status);
+    printf("Child process ended\n");
+  }*/
+  if(0>child) return EXIT_FAILURE;
+  if (child>0) {
+    int status;
+    printf("I'm the parent\n");
+    printf("Waits 2 seconds, then send a SIGTERM %i signal (it is blocked)\n",SIGTERM);
+    sleep(2);
+    kill(child,SIGTERM);
+    printf("I sent it.\n");
+  }
+  else
+  {
+    sigemptyset(&sigset); //the child has a different addr-space
+                          //so this won't affect the parent
+    printf("I'm the child waiting for signal.\n");
+    sigsuspend(&sigset);
+    printf("Child process ended\n");
+
+  }
+  return 0;
+}
